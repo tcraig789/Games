9460d9b9120fb8f73e0dd04ba9338123f25bf252
Crisandwitch <crisandwitch@gmail.com>
2014-01-27 15:41:58 -0500

    Added Rotator Code

---
+++ BallMazeOfficial/Assets/Assets/Scrips/RotatorChar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorChar : MonoBehaviour {
+	private GameObject downRotate;
+	public float zSpeed;
+	public float ySpeed;
+	public float xSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.UpArrow)){
+			zSpeed++;
+		}
+
+		else if (Input.GetKey(KeyCode.DownArrow)){
+			zSpeed--;
+		}
+
+		else if (Input.GetKey(KeyCode.LeftArrow)){
+			xSpeed--;
+		}
+
+		else if (Input.GetKey(KeyCode.RightArrow)){
+			xSpeed++;
+		}
+		else {
+			zSpeed = 0;
+			ySpeed = 0;
+			xSpeed = 0;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			zSpeed = 0;
+		} 
+	}
+}
